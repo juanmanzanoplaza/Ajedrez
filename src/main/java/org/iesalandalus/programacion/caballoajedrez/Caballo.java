d64acb8401644405fa427751afebdc36fbdc40b4
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-12 15:52:28 +0100

    Realizado punto 14

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -9,6 +9,17 @@ public class Caballo {
 		setColor(Color.NEGRO);
 		setPosicion(new Posicion(8,'b'));
 	}
+	
+	//Constructor con color
+	public Caballo(Color color) {
+		if(color == null)
+			throw new IllegalArgumentException();
+		setColor(color);
+		if(color.name().equals("BLANCO"))
+			setPosicion(new Posicion(1, 'b'));
+		else
+			setPosicion(new Posicion(8, 'b'));
+	}
 
 	public Color getColor() {
 		return color;
