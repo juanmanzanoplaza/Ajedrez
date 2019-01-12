d8461817ebc10e68f44112b1bbe5a1e2ac75cd37
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-12 13:49:54 +0100

    Realizado punto 06

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,4 +4,25 @@ public class Posicion {
 	private int fila;
 	private char columna;
 
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) throws IllegalArgumentException{
+		if(fila<1 || fila>8)
+			throw new IllegalArgumentException("ERROR: El número de fila debe estar entre 1 y 8.");
+		this.fila = fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) throws IllegalArgumentException{
+		if(columna!='a' && columna!='b' && columna!='c' && columna!='d'
+				&& columna!='e' && columna!='f' && columna!='g' && columna!='h')
+			throw new IllegalArgumentException("ERROR: El carácter de columna debe estar entre a y h.");
+		this.columna = columna;
+	}
+
 }
