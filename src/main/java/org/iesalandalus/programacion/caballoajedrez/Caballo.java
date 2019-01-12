73269a4240fdc02c2a0b38ed2bce5cb21b7aa6ed
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-12 15:39:14 +0100

    Realizado punto 12

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -3,5 +3,25 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Caballo {
 	private Color color;
 	private Posicion posicion;
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	private void setColor(Color color) {
+		if(color == null)
+			throw new IllegalArgumentException("");
+		this.color = color;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setPosicion(Posicion posicion) {
+		if(posicion == null)
+			throw new IllegalArgumentException("");
+		this.posicion = posicion;
+	}
 
 }
