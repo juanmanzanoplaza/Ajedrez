750147a8237fb8085c47007d1e5572206c5bea23
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-12 15:41:00 +0100

    Realizado punto 13

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -3,21 +3,27 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Caballo {
 	private Color color;
 	private Posicion posicion;
-	
+
+	//Constructor por defecto
+	public Caballo() {
+		setColor(Color.NEGRO);
+		setPosicion(new Posicion(8,'b'));
+	}
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	private void setColor(Color color) {
 		if(color == null)
 			throw new IllegalArgumentException("");
 		this.color = color;
 	}
-	
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
-	
+
 	private void setPosicion(Posicion posicion) {
 		if(posicion == null)
 			throw new IllegalArgumentException("");
